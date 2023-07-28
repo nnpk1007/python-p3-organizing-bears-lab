d00fa3ea17ad19e289ca113a77325e837e500fcb
Ngoc Nguyen <nnpk0410@gmail.com>
2023-07-28 15:09:50 -0500

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive)
+VALUES (1, "Mr. Chocolate", 5, "M", "brown", "calm", 1),
+(2, "Rowdy", 2, "M", "white", "friendly", 1),
+(3, "Tabitha", 10, "F", "black", "lovely", 0),
+(4, "Sergeant Brown", 7, "M", "black", "kissy", 1),
+(5, "Melissa", 2, "F", "white", "nice", 1),
+(6, "Grinch", 3, "M", "brown", "angry", 1),
+(7, "Wendy", 4, "F", "black", "beauty", 1),
+(8, NULL, 5, "F", "white", "calm", 1);
